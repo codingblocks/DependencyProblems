752fd7a104f3d8343bd896f21a4fb1813a4c5016
allen <allen>
2016-12-18 22:27:20 -0500

    Initial commit - dependency problems there

---
+++ Core.Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Core.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class Person
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Person));
+
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Gender Gender;
+
+        public Person(string firstName, string lastName, int age, string gender)
+        {
+            Gender parseGender;
+
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+
+            var parsedSuccessfully = Enum.TryParse(gender, true, out parseGender);
+            if (parsedSuccessfully)
+            {
+                Gender = parseGender;
+            }
+
+            log.Info(this);
+        }
+    }
+}
---
+++ DependencyProblems/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace DependencyProblems
+{
+    class Program
+    {
+        // Define a static logger variable so that it references the
+        // Logger instance named "MyApp".
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new FileInfo("logging.config"));
+
+            log.Warn("test");
+            log.Error("Just a test");
+            Console.Write("Test");
+            Console.ReadKey();
+
+            var person = new Core.Models.Person("Allen", "Underwood", 21, "Male");
+            Console.ReadKey();
+        }
+    }
+}
